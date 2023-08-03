a780fc48b1b623c23f11ff6e7eecd572d0ac5880
Mitchell Hayes <mitchellhayes95@outlook.com>
2023-08-03 15:31:25 -0400

    updated link to coffeeApp

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -5,7 +5,7 @@ const ProjectsSection: React.FC = () => {
     <section id="projects" className="projects-section">
       <h2 className="projects-section-header">Some of my projects</h2>
       <div className="projects-grid">
-        <a href="https://mitchellhayes.github.io/coffee-app/" target="_blank" rel="noreferrer" className="project project-tile">
+        <a href="https://coffee.hayeshomelab.com" target="_blank" rel="noreferrer" className="project project-tile">
           <img className="project-image" src="./assets/media/coffeeApp.PNG" alt="Coffee App Project" />
           <p className="project-title">Coffee App</p>
         </a>
